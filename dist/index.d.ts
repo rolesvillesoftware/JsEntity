29b8419aa210ab9d045aeb4f529caa65bb4c7167
Rolesville Software LLC <github@rolesvillesoftware.com>
2018-02-17 01:08:12 -0500

    Fixed ContextModel missing

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,5 @@
+export { Connection } from "./src/Connection";
 export { Context } from "./src/Context";
+export { ContextModel } from "./src/ContextModel";
 export { DbSet } from "./src/DbSet";
 export { Entity } from "./src/Entity";
-export { Connection } from "./src/Connection";
